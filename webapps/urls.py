3b936b15c4a34b84941675a033e680cd290e634d
Hardik Jain <hardikashishjain@gmail.com>
2021-03-18 21:44:27 +0530

    [url] Redirect to JoinView for any url except for those allowed.

--- webapps/urls.py
+++ webapps/urls.py
@@ -17,11 +17,15 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
+from django.views.generic import RedirectView
+
+from monopoly.views.join_view import JoinView
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^monopoly/', include('monopoly.urls')),
-    url(r'^admin/', include(admin.site.urls)), 
-    url('^social/', include('social_django.urls', namespace='social'))
+    url(r'^admin/', include(admin.site.urls)),
+    url('^social/', include('social_django.urls', namespace='social')),
+    url(r'^.*$', RedirectView.as_view(pattern_name='join', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
