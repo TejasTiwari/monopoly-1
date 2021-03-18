41ebbf5e9f926bfdd6d0e2a3ab51be55037eb1c4
pranay-simejia <simejiapranay@gmail.com>
2021-03-18 14:08:17 +0530

    update

--- monopoly/static/js/views/game.js
+++ monopoly/static/js/views/game.js
@@ -353,6 +353,7 @@ class GameView {
     }
 
     handleTrade = function (message) {
+        console.log(message)
         document.getElementById('trade').style.display = 'inherit';
         document.getElementsByClassName('card-content-container')[0].style.display = 'none';
         document.getElementById('accepttradebutton').style.display = 'none';
@@ -566,7 +567,7 @@ class GameView {
     }
 
     async trade (){
-        console.log(this)
+        // console.log(this)
         this.socket.send(JSON.stringify({
             action: "trade",
             hostname: this.hostName,
