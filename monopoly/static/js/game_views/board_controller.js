35ce62977920decce993c09d5d65792322c8b6ce
unknown <bhowmicksoham2001@gmail.com>
2021-03-20 14:18:38 +0530

    added modal pictures

--- monopoly/static/js/game_views/board_controller.js
+++ monopoly/static/js/game_views/board_controller.js
@@ -306,7 +306,7 @@ class BoardController {
                                         modalElem.innerHTML = `<h1>${modalData.name}</h1>`
                                         modalElem.innerHTML += `<h3>${modalData.desc}</h3>`
                                         modalElem.innerHTML += `<h4>${modalData.cost}</h4>`
-                                        modalElem.innerHTML += `<img src='../static/images/${modelData.name}.jpg'>`
+                                        modalElem.innerHTML += `<img src='../static/images/modal_pics/${modalData.name}'>`
                                     })
                                 }
                               },
